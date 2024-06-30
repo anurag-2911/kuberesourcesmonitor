a0cd38adc109e392429c556cf4324b01e6be2e77
anurag <anurag@ubuntu-01.myguest.virtualbox.org>
2024-06-30 21:43:30 +0530

    crd and controller implementation

--- internal/controller/kuberesourcesmonitor_controller.go
+++ internal/controller/kuberesourcesmonitor_controller.go
@@ -1,62 +1,121 @@
-/*
-Copyright 2024.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package controller
 
 import (
-	"context"
-
-	"k8s.io/apimachinery/pkg/runtime"
-	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/log"
-
-	monitorv1alpha1 "github.com/anurag-2911/kuberesourcesmonitor/api/v1alpha1"
+    "context"
+    "time"
+    "github.com/go-logr/logr"
+    "github.com/prometheus/client_golang/prometheus"
+    "github.com/prometheus/client_golang/prometheus/push"
+    "k8s.io/apimachinery/pkg/api/errors"
+    "k8s.io/apimachinery/pkg/runtime"
+    corev1 "k8s.io/api/core/v1"
+    "sigs.k8s.io/controller-runtime/pkg/client"
+    
+    "sigs.k8s.io/controller-runtime/pkg/reconcile"
+    ctrl "sigs.k8s.io/controller-runtime"
+    monitorv1alpha1 "github.com/anurag-2911/kuberesourcesmonitor/api/v1alpha1"
 )
 
 // KubeResourcesMonitorReconciler reconciles a KubeResourcesMonitor object
 type KubeResourcesMonitorReconciler struct {
-	client.Client
-	Scheme *runtime.Scheme
+    client.Client
+    Scheme *runtime.Scheme
+    Log    logr.Logger
 }
 
-//+kubebuilder:rbac:groups=monitor.example.com,resources=kuberesourcesmonitors,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=monitor.example.com,resources=kuberesourcesmonitors/status,verbs=get;update;patch
-//+kubebuilder:rbac:groups=monitor.example.com,resources=kuberesourcesmonitors/finalizers,verbs=update
+// +kubebuilder:rbac:groups=monitor.example.com,resources=kuberesourcesmonitors,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=monitor.example.com,resources=kuberesourcesmonitors/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=monitor.example.com,resources=kuberesourcesmonitors/finalizers,verbs=update
+// +kubebuilder:rbac:groups="",resources=pods;services;configmaps;secrets,verbs=get;list
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the KubeResourcesMonitor object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.16.3/pkg/reconcile
-func (r *KubeResourcesMonitorReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+func (r *KubeResourcesMonitorReconciler) Reconcile(ctx context.Context, req reconcile.Request) (reconcile.Result, error) {
+    log := r.Log.WithValues("kuberesourcesmonitor", req.NamespacedName)
 
-	// TODO(user): your logic here
+    // Fetch the KubeResourcesMonitor instance
+    instance := &monitorv1alpha1.KubeResourcesMonitor{}
+    err := r.Get(ctx, req.NamespacedName, instance)
+    if err != nil {
+        if errors.IsNotFound(err) {
+            return reconcile.Result{}, nil
+        }
+        return reconcile.Result{}, err
+    }
 
-	return ctrl.Result{}, nil
+    // Define a new Pod object
+    podList := &corev1.PodList{}
+    err = r.Client.List(ctx, podList)
+    if err != nil {
+        return reconcile.Result{}, err
+    }
+
+    // Get the counts of resources
+    podCount := len(podList.Items)
+    
+    serviceList := &corev1.ServiceList{}
+    err = r.Client.List(ctx, serviceList)
+    if err != nil {
+        return reconcile.Result{}, err
+    }
+    serviceCount := len(serviceList.Items)
+    
+    configMapList := &corev1.ConfigMapList{}
+    err = r.Client.List(ctx, configMapList)
+    if err != nil {
+        return reconcile.Result{}, err
+    }
+    configMapCount := len(configMapList.Items)
+    
+    secretList := &corev1.SecretList{}
+    err = r.Client.List(ctx, secretList)
+    if err != nil {
+        return reconcile.Result{}, err
+    }
+    secretCount := len(secretList.Items)
+
+    // Log the counts
+    log.Info("Resource counts", "Pods", podCount, "Services", serviceCount, "ConfigMaps", configMapCount, "Secrets", secretCount)
+
+    // Define the gauges
+    podGauge := prometheus.NewGauge(prometheus.GaugeOpts{
+        Name: "pod_count",
+        Help: "Number of pods",
+    })
+    serviceGauge := prometheus.NewGauge(prometheus.GaugeOpts{
+        Name: "service_count",
+        Help: "Number of services",
+    })
+    configMapGauge := prometheus.NewGauge(prometheus.GaugeOpts{
+        Name: "configmap_count",
+        Help: "Number of configmaps",
+    })
+    secretGauge := prometheus.NewGauge(prometheus.GaugeOpts{
+        Name: "secret_count",
+        Help: "Number of secrets",
+    })
+
+    // Set the gauge values
+    podGauge.Set(float64(podCount))
+    serviceGauge.Set(float64(serviceCount))
+    configMapGauge.Set(float64(configMapCount))
+    secretGauge.Set(float64(secretCount))
+
+    // Push metrics to Prometheus
+    pusher := push.New("http://prometheus:9091", "kuberesourcesmonitor").
+        Collector(podGauge).
+        Collector(serviceGauge).
+        Collector(configMapGauge).
+        Collector(secretGauge)
+
+    if err := pusher.Push(); err != nil {
+        log.Error(err, "Could not push to Prometheus")
+    }
+
+    return reconcile.Result{RequeueAfter: time.Minute * 5}, nil
 }
 
-// SetupWithManager sets up the controller with the Manager.
 func (r *KubeResourcesMonitorReconciler) SetupWithManager(mgr ctrl.Manager) error {
-	return ctrl.NewControllerManagedBy(mgr).
-		For(&monitorv1alpha1.KubeResourcesMonitor{}).
-		Complete(r)
+    return ctrl.NewControllerManagedBy(mgr).
+        For(&monitorv1alpha1.KubeResourcesMonitor{}).
+        Complete(r)
 }
+
