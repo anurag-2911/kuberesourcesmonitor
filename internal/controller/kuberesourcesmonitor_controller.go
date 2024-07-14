a9d6ebba9293b2392c25a42a21aa01a611b0a777
anurag <anurag@ubuntu-01.myguest.virtualbox.org>
2024-07-14 14:25:18 +0530

    configmap based metric collection

--- internal/controller/kuberesourcesmonitor_controller.go
+++ internal/controller/kuberesourcesmonitor_controller.go
@@ -2,11 +2,11 @@ package controller
 
 import (
 	"context"
+	"strings"
 	"time"
 
 	"github.com/go-logr/logr"
 	"github.com/prometheus/client_golang/prometheus"
-
 	"github.com/prometheus/client_golang/prometheus/push"
 	appsv1 "k8s.io/api/apps/v1"
 	batchv1 "k8s.io/api/batch/v1"
@@ -30,14 +30,13 @@ type KubeResourcesMonitorReconciler struct {
 // +kubebuilder:rbac:groups=monitor.example.com,resources=kuberesourcesmonitors,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=monitor.example.com,resources=kuberesourcesmonitors/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=monitor.example.com,resources=kuberesourcesmonitors/finalizers,verbs=update
-// +kubebuilder:rbac:groups="",resources=pods;services;configmaps;secrets;cronjobs;jobs;deployments;nodes;persistentvolumeclaims;events,verbs=get;list
+// +kubebuilder:rbac:groups="",resources=pods;services;configmaps;secrets;cronjobs;jobs;deployments;nodes;persistentvolumeclaims;events;daemonsets;replicasets;replicationcontrollers;statefulsets,verbs=get;list
 
 func (r *KubeResourcesMonitorReconciler) Reconcile(ctx context.Context, req reconcile.Request) (reconcile.Result, error) {
 	log := r.Log.WithValues("kuberesourcesmonitor", req.NamespacedName)
 	log.Info("Reconcile function called")
 	log.Info("Reconciling KubeResourcesMonitor", "namespace", req.Namespace, "name", req.Name)
 
-	// Fetch the KubeResourcesMonitor instance
 	instance := &monitorv1alpha1.KubeResourcesMonitor{}
 	err := r.Get(ctx, req.NamespacedName, instance)
 	if err != nil {
@@ -48,120 +47,171 @@ func (r *KubeResourcesMonitorReconciler) Reconcile(ctx context.Context, req reco
 		log.Error(err, "Failed to get KubeResourcesMonitor")
 		return reconcile.Result{}, err
 	}
-	// Get the Prometheus endpoint from the spec
+
 	prometheusEndpoint := instance.Spec.PrometheusEndpoint
 
-	// Collect resource metrics
+	configMap := &corev1.ConfigMap{}
+	err = r.Client.Get(ctx, client.ObjectKey{Namespace: req.Namespace, Name: "kuberesourcesmonitor-config"}, configMap)
+	if err != nil {
+		log.Error(err, "Failed to get ConfigMap")
+		return reconcile.Result{}, err
+	}
+
+	metricsToCollect := strings.Split(configMap.Data["metrics"], "\n")
+
+	var (
+		podCount, serviceCount, configMapCount, secretCount            int
+		cronJobCount, jobCount, daemonSetCount, replicaSetCount        int
+		replicationControllerCount, statefulSetCount, deploymentCount  int
+		nodeCount, pvcCount, eventCount, restartCount, crashCount      int
+		totalCPUUsage, totalMemoryUsage, nodeReady, nodeMemoryPressure float64
+		nodeDiskPressure                                               float64
+	)
+
 	podList := &corev1.PodList{}
 	err = r.Client.List(ctx, podList)
 	if err != nil {
 		return reconcile.Result{}, err
 	}
-	podCount := len(podList.Items)
 
-	serviceList := &corev1.ServiceList{}
-	err = r.Client.List(ctx, serviceList)
-	if err != nil {
-		return reconcile.Result{}, err
-	}
-	serviceCount := len(serviceList.Items)
-
-	configMapList := &corev1.ConfigMapList{}
-	err = r.Client.List(ctx, configMapList)
-	if err != nil {
-		return reconcile.Result{}, err
-	}
-	configMapCount := len(configMapList.Items)
-
-	secretList := &corev1.SecretList{}
-	err = r.Client.List(ctx, secretList)
-	if err != nil {
-		return reconcile.Result{}, err
-	}
-	secretCount := len(secretList.Items)
-
-	cronJobList := &batchv1.CronJobList{}
-	err = r.Client.List(ctx, cronJobList)
-	if err != nil {
-		return reconcile.Result{}, err
-	}
-	cronJobCount := len(cronJobList.Items)
-
-	deploymentList := &appsv1.DeploymentList{}
-	err = r.Client.List(ctx, deploymentList)
-	if err != nil {
-		return reconcile.Result{}, err
-	}
-	deploymentCount := len(deploymentList.Items)
-
-	nodeList := &corev1.NodeList{}
-	err = r.Client.List(ctx, nodeList)
-	if err != nil {
-		return reconcile.Result{}, err
-	}
-	nodeCount := len(nodeList.Items)
-
-	pvcList := &corev1.PersistentVolumeClaimList{}
-	err = r.Client.List(ctx, pvcList)
-	if err != nil {
-		return reconcile.Result{}, err
-	}
-	pvcCount := len(pvcList.Items)
-
-	eventList := &corev1.EventList{}
-	err = r.Client.List(ctx, eventList)
-	if err != nil {
-		return reconcile.Result{}, err
-	}
-	eventCount := len(eventList.Items)
-
-	var restartCount int
-	var crashCount int
-	for _, pod := range podList.Items {
-		for _, status := range pod.Status.ContainerStatuses {
-			restartCount += int(status.RestartCount)
-			if status.State.Terminated != nil && status.State.Terminated.ExitCode != 0 {
-				crashCount++
+	for _, metric := range metricsToCollect {
+		switch metric {
+		case "pods":
+			podCount = len(podList.Items)
+		case "services":
+			serviceList := &corev1.ServiceList{}
+			err = r.Client.List(ctx, serviceList)
+			if err != nil {
+				return reconcile.Result{}, err
+			}
+			serviceCount = len(serviceList.Items)
+		case "configmaps":
+			configMapList := &corev1.ConfigMapList{}
+			err = r.Client.List(ctx, configMapList)
+			if err != nil {
+				return reconcile.Result{}, err
+			}
+			configMapCount = len(configMapList.Items)
+		case "secrets":
+			secretList := &corev1.SecretList{}
+			err = r.Client.List(ctx, secretList)
+			if err != nil {
+				return reconcile.Result{}, err
+			}
+			secretCount = len(secretList.Items)
+		case "cronjobs":
+			cronJobList := &batchv1.CronJobList{}
+			err = r.Client.List(ctx, cronJobList)
+			if err != nil {
+				return reconcile.Result{}, err
+			}
+			cronJobCount = len(cronJobList.Items)
+		case "jobs":
+			jobList := &batchv1.JobList{}
+			err = r.Client.List(ctx, jobList)
+			if err != nil {
+				return reconcile.Result{}, err
+			}
+			jobCount = len(jobList.Items)
+		case "daemonsets":
+			daemonSetList := &appsv1.DaemonSetList{}
+			err = r.Client.List(ctx, daemonSetList)
+			if err != nil {
+				return reconcile.Result{}, err
+			}
+			daemonSetCount = len(daemonSetList.Items)
+		case "replicasets":
+			replicaSetList := &appsv1.ReplicaSetList{}
+			err = r.Client.List(ctx, replicaSetList)
+			if err != nil {
+				return reconcile.Result{}, err
+			}
+			replicaSetCount = len(replicaSetList.Items)
+		case "replicationcontrollers":
+			replicationControllerList := &corev1.ReplicationControllerList{}
+			err = r.Client.List(ctx, replicationControllerList)
+			if err != nil {
+				return reconcile.Result{}, err
+			}
+			replicationControllerCount = len(replicationControllerList.Items)
+		case "statefulsets":
+			statefulSetList := &appsv1.StatefulSetList{}
+			err = r.Client.List(ctx, statefulSetList)
+			if err != nil {
+				return reconcile.Result{}, err
+			}
+			statefulSetCount = len(statefulSetList.Items)
+		case "deployments":
+			deploymentList := &appsv1.DeploymentList{}
+			err = r.Client.List(ctx, deploymentList)
+			if err != nil {
+				return reconcile.Result{}, err
+			}
+			deploymentCount = len(deploymentList.Items)
+		case "nodes":
+			nodeList := &corev1.NodeList{}
+			err = r.Client.List(ctx, nodeList)
+			if err != nil {
+				return reconcile.Result{}, err
+			}
+			nodeCount = len(nodeList.Items)
+			for _, node := range nodeList.Items {
+				for _, condition := range node.Status.Conditions {
+					switch condition.Type {
+					case corev1.NodeReady:
+						if condition.Status == corev1.ConditionTrue {
+							nodeReady++
+						}
+					case corev1.NodeMemoryPressure:
+						if condition.Status == corev1.ConditionTrue {
+							nodeMemoryPressure++
+						}
+					case corev1.NodeDiskPressure:
+						if condition.Status == corev1.ConditionTrue {
+							nodeDiskPressure++
+						}
+					}
+				}
+				for resourceName, allocatable := range node.Status.Allocatable {
+					if resourceName == corev1.ResourceCPU {
+						totalCPUUsage += float64(allocatable.MilliValue()) / 1000
+					} else if resourceName == corev1.ResourceMemory {
+						totalMemoryUsage += float64(allocatable.Value()) / (1024 * 1024 * 1024)
+					}
+				}
+			}
+		case "pvcs":
+			pvcList := &corev1.PersistentVolumeClaimList{}
+			err = r.Client.List(ctx, pvcList)
+			if err != nil {
+				return reconcile.Result{}, err
+			}
+			pvcCount = len(pvcList.Items)
+		case "events":
+			eventList := &corev1.EventList{}
+			err = r.Client.List(ctx, eventList)
+			if err != nil {
+				return reconcile.Result{}, err
+			}
+			eventCount = len(eventList.Items)
+		case "restarts":
+			for _, pod := range podList.Items {
+				for _, status := range pod.Status.ContainerStatuses {
+					restartCount += int(status.RestartCount)
+				}
+			}
+		case "crashes":
+			for _, pod := range podList.Items {
+				for _, status := range pod.Status.ContainerStatuses {
+					if status.State.Terminated != nil && status.State.Terminated.ExitCode != 0 {
+						crashCount++
+					}
+				}
 			}
 		}
 	}
-	// Node resource usage and conditions
-	var totalCPUUsageMilli, totalMemoryUsageBytes float64
-	var nodeReady, nodeMemoryPressure, nodeDiskPressure float64
-	for _, node := range nodeList.Items {
-		for _, condition := range node.Status.Conditions {
-			switch condition.Type {
-			case corev1.NodeReady:
-				if condition.Status == corev1.ConditionTrue {
-					nodeReady++
-				}
-			case corev1.NodeMemoryPressure:
-				if condition.Status == corev1.ConditionTrue {
-					nodeMemoryPressure++
-				}
-			case corev1.NodeDiskPressure:
-				if condition.Status == corev1.ConditionTrue {
-					nodeDiskPressure++
-				}
-			}
-		}
-		for resourceName, allocatable := range node.Status.Allocatable {
-			if resourceName == corev1.ResourceCPU {
-				totalCPUUsageMilli += float64(allocatable.MilliValue())
-			} else if resourceName == corev1.ResourceMemory {
-				totalMemoryUsageBytes += float64(allocatable.Value())
-			}
-		}
-	}
 
-	// Convert CPU usage from milliCPUs to CPUs
-	totalCPUUsage := totalCPUUsageMilli / 1000.0
-
-	// Convert memory usage from bytes to gigabytes
-	totalMemoryUsage := totalMemoryUsageBytes / (1024.0 * 1024.0 * 1024.0)
-
-	// Log the counts with improved readability
-	log.Info("Resource counts", "Pods", podCount, "Services", serviceCount, "ConfigMaps", configMapCount, "Secrets", secretCount, "CronJobs", cronJobCount, "Deployments", deploymentCount, "Nodes", nodeCount, "PVCs", pvcCount, "Events", eventCount, "Restarts", restartCount, "Crashes", crashCount, "TotalCPUUsage (cores)", totalCPUUsage, "TotalMemoryUsage (GB)", totalMemoryUsage, "NodeReady", nodeReady, "NodeMemoryPressure", nodeMemoryPressure, "NodeDiskPressure", nodeDiskPressure)
+	log.Info("Resource counts", "Pods", podCount, "Services", serviceCount, "ConfigMaps", configMapCount, "Secrets", secretCount, "CronJobs", cronJobCount, "Jobs", jobCount, "DaemonSets", daemonSetCount, "ReplicaSets", replicaSetCount, "ReplicationControllers", replicationControllerCount, "StatefulSets", statefulSetCount, "Deployments", deploymentCount, "Nodes", nodeCount, "PVCs", pvcCount, "Events", eventCount, "Restarts", restartCount, "Crashes", crashCount, "TotalCPUUsage (cores)", totalCPUUsage, "TotalMemoryUsage (GB)", totalMemoryUsage, "NodeReady", nodeReady, "NodeMemoryPressure", nodeMemoryPressure, "NodeDiskPressure", nodeDiskPressure)
 
 	// Define the gauges
 	podGauge := prometheus.NewGauge(prometheus.GaugeOpts{
@@ -184,6 +234,26 @@ func (r *KubeResourcesMonitorReconciler) Reconcile(ctx context.Context, req reco
 		Name: "cronjob_count",
 		Help: "Number of cronjobs",
 	})
+	jobGauge := prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "job_count",
+		Help: "Number of jobs",
+	})
+	daemonSetGauge := prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "daemonset_count",
+		Help: "Number of daemonsets",
+	})
+	replicaSetGauge := prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "replicaset_count",
+		Help: "Number of replicasets",
+	})
+	replicationControllerGauge := prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "replicationcontroller_count",
+		Help: "Number of replication controllers",
+	})
+	statefulSetGauge := prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "statefulset_count",
+		Help: "Number of stateful sets",
+	})
 	deploymentGauge := prometheus.NewGauge(prometheus.GaugeOpts{
 		Name: "deployment_count",
 		Help: "Number of deployments",
@@ -191,18 +261,6 @@ func (r *KubeResourcesMonitorReconciler) Reconcile(ctx context.Context, req reco
 	nodeGauge := prometheus.NewGauge(prometheus.GaugeOpts{
 		Name: "node_count",
 		Help: "Number of nodes",
-	})
-	nodeReadyGauge := prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "node_ready_count",
-		Help: "Number of nodes in ready condition",
-	})
-	nodeMemoryPressureGauge := prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "node_memory_pressure_count",
-		Help: "Number of nodes with memory pressure",
-	})
-	nodeDiskPressureGauge := prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "node_disk_pressure_count",
-		Help: "Number of nodes with disk pressure",
 	})
 	pvcGauge := prometheus.NewGauge(prometheus.GaugeOpts{
 		Name: "pvc_count",
@@ -228,52 +286,72 @@ func (r *KubeResourcesMonitorReconciler) Reconcile(ctx context.Context, req reco
 		Name: "total_memory_usage",
 		Help: "Total memory usage across all nodes",
 	})
+	nodeReadyGauge := prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "node_ready_count",
+		Help: "Number of nodes in ready condition",
+	})
+	nodeMemoryPressureGauge := prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "node_memory_pressure_count",
+		Help: "Number of nodes with memory pressure",
+	})
+	nodeDiskPressureGauge := prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "node_disk_pressure_count",
+		Help: "Number of nodes with disk pressure",
+	})
 
-	// Set the gauge values
 	podGauge.Set(float64(podCount))
 	serviceGauge.Set(float64(serviceCount))
 	configMapGauge.Set(float64(configMapCount))
 	secretGauge.Set(float64(secretCount))
 	cronJobGauge.Set(float64(cronJobCount))
+	jobGauge.Set(float64(jobCount))
+	daemonSetGauge.Set(float64(daemonSetCount))
+	replicaSetGauge.Set(float64(replicaSetCount))
+	replicationControllerGauge.Set(float64(replicationControllerCount))
+	statefulSetGauge.Set(float64(statefulSetCount))
 	deploymentGauge.Set(float64(deploymentCount))
 	nodeGauge.Set(float64(nodeCount))
-	nodeReadyGauge.Set(nodeReady)
-	nodeMemoryPressureGauge.Set(nodeMemoryPressure)
-	nodeDiskPressureGauge.Set(nodeDiskPressure)
 	pvcGauge.Set(float64(pvcCount))
 	eventGauge.Set(float64(eventCount))
 	restartGauge.Set(float64(restartCount))
 	crashGauge.Set(float64(crashCount))
 	cpuUsageGauge.Set(totalCPUUsage)
 	memoryUsageGauge.Set(totalMemoryUsage)
+	nodeReadyGauge.Set(nodeReady)
+	nodeMemoryPressureGauge.Set(nodeMemoryPressure)
+	nodeDiskPressureGauge.Set(nodeDiskPressure)
 
-	// Push metrics to Prometheus Pushgateway
 	pusher := push.New(prometheusEndpoint, "kuberesourcesmonitor").
 		Collector(podGauge).
 		Collector(serviceGauge).
 		Collector(configMapGauge).
 		Collector(secretGauge).
 		Collector(cronJobGauge).
+		Collector(jobGauge).
+		Collector(daemonSetGauge).
+		Collector(replicaSetGauge).
+		Collector(replicationControllerGauge).
+		Collector(statefulSetGauge).
 		Collector(deploymentGauge).
 		Collector(nodeGauge).
-		Collector(nodeReadyGauge).
-		Collector(nodeMemoryPressureGauge).
-		Collector(nodeDiskPressureGauge).
 		Collector(pvcGauge).
 		Collector(eventGauge).
 		Collector(restartGauge).
 		Collector(crashGauge).
 		Collector(cpuUsageGauge).
-		Collector(memoryUsageGauge)
+		Collector(memoryUsageGauge).
+		Collector(nodeReadyGauge).
+		Collector(nodeMemoryPressureGauge).
+		Collector(nodeDiskPressureGauge)
 
 	if err := pusher.Push(); err != nil {
 		log.Error(err, "Could not push to Prometheus Pushgateway")
 	} else {
 		log.Info("Successfully pushed metrics to Prometheus Pushgateway")
 	}
+
 	log.Info("Successfully reconciled KubeResourcesMonitor")
 
-	// Parse interval from the instance spec
 	timeInterval := instance.Spec.Interval
 	interval := 5 * time.Minute // default interval
 
@@ -291,8 +369,6 @@ func (r *KubeResourcesMonitorReconciler) Reconcile(ctx context.Context, req reco
 
 func (r *KubeResourcesMonitorReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	r.Log.Info("SetupWithManager called")
-	// Expose the metrics endpoint
-
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&monitorv1alpha1.KubeResourcesMonitor{}).
 		Complete(r)
